4ad098b3b6ea508d237f2b5813837be76fb59abc
David T. John <davidjohn_atl@yahoo.com>
2021-10-26 20:57:19 -0400

    updated header wc content and style

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-class Header extends HTMLElement {
-    super();
-    constructor(){
+const template = document.createElement('template');
+template.innerHTML = `
+    <style> 
+        nav{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: space-between;
+        }
+        h1{
+            color: blueviolet;
+        }
+        ul{
+            list-style: none;
+            display: flex;
+            flex-direction: row;
+        }
+        li{
+            padding: 0rem 1rem;
+        }
+
+    </style>
+    <nav>
+        <h1>Logo</h1>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </nav>
+`;
+
+class WC_Header extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true))       
         
     }
 }
+window.customElements.define('wc-header', WC_Header)
